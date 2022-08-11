de45751d5b955ca3c8a631b6f216f50fac7c80cd
Leanne Macaspac <leanne@macaspac.ca>
2022-08-11 14:16:25 -0400

    copy + css changes

---
+++ libraries/draggable.js
@@ -0,0 +1,18 @@
+//Select draggable item
+const draggable = new Draggable.default(document.querySelector('img'), {
+  draggable: '.pixel-art'
+});
+
+const 
+
+//Click and hold draggable item to enable dragging
+function draggable.on('drag:start', (dragEvent) => mousedown() {
+  console.log('start');
+}
+
+//Move item as my mouse moves
+
+//Release the item when I let go of the mouse
+function dragEnd() {
+  console.log('end');
+}
